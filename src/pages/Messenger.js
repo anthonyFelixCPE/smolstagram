b06e78d70936bc080b7c836771216002f8f41886
Lloren <storageaccntone@gmail.com>
2025-03-18 14:01:35 +0800

    working image sending with minor fixes

--- src/pages/Messenger.js
+++ src/pages/Messenger.js
@@ -48,36 +48,6 @@ function Messenger() {
 
     const [selectedImages, setSelectedImages] = useState([]);
 
-    const handleImageUpload = (event) => {
-        const files = event.target.files;
-        if (files.length > 0) {
-            const imagesArray = [];
-            let oversizedFiles = false;
-
-            Array.from(files).forEach((file) => {
-                if (file.size > 1024 * 1024) {
-                    oversizedFiles = true;
-                } else if (
-                    file.type === "image/jpeg" ||
-                    file.type === "image/png"
-                ) {
-                    const reader = new FileReader();
-                    reader.onloadend = () => {
-                        imagesArray.push(reader.result);
-                        setSelectedImages([...selectedImages, ...imagesArray]);
-                    };
-                    reader.readAsDataURL(file);
-                }
-            });
-
-            if (oversizedFiles) {
-                alert(
-                    "One or more images exceed 1MB and will not be uploaded."
-                );
-            }
-        }
-    };
-
     useEffect(() => {
         const storedUser = localStorage.getItem("user");
         if (storedUser) {
